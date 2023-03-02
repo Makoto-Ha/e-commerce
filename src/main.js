4da5dadcc64376d9363c757ad8e8c414b9631458
User <makoto@gmail.com>
2023-03-02 17:24:27 +0800

    新增模組拆分

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from '@/router';
+import TypeNav from '@/pages/Home/TypeNav';
 Vue.config.productionTip = false;
-
+Vue.component(TypeNav.name, TypeNav);
+import { reqCategoryList } from './api';
+console.log(reqCategoryList());
 new Vue({
   render: h => h(App),
   router
